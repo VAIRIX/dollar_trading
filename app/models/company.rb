4e8fa4af08aaf2ef29fe0360903fbc8b52f11be2
Gaston Arbeletche -VAIRIX- <garbeletche@vairix.com>
2014-01-31 17:36:33 -0200

    refactor method save exchange_reate

--- app/models/company.rb
+++ app/models/company.rb
@@ -45,72 +45,42 @@ class Company < ActiveRecord::Base
     rate = agent.page.search(self.search)[0].text.strip.split("\r\n")
     purchase_value = rate[1].gsub(',','.').to_f
     sale_value = rate[2].gsub(',','.').to_f
-    average_value = ((purchase_value + sale_value)/2).round(2)
-    exchangeRate = ExchangeRate.new(company_id:self.id, currency_id: Currency::DollarID,purchase: purchase_value, sale: sale_value, average: average_value)
-    last_exchangeRate = ExchangeRate.last_of_the_company(self.id)
-    if (last_exchangeRate.nil? || !last_exchangeRate.equals?(exchangeRate))
-      exchangeRate.save
-    end
+    ExchangeRate.exchange_dollar(purchase_value,sale_value, self)
   end
 
   def cambio_18(agent)
     rate = agent.page.search(self.search)[4].text.split('Dólar USA')[1].split('Euros')[0]
     purchase_value = rate.gsub("\n\t",'').gsub("\n",'').strip.split("\t\t\t\t\t\t\t")[0].to_f
     sale_value = rate.gsub("\n\t",'').gsub("\n",'').strip.split("\t\t\t\t\t\t\t")[1].to_f
-    average_value = ((purchase_value + sale_value)/2).round(2)
-    exchangeRate = ExchangeRate.new(company_id:self.id, currency_id: Currency::DollarID,purchase: purchase_value, sale: sale_value, average: average_value)
-    last_exchangeRate = ExchangeRate.last_of_the_company(self.id)
-    if (last_exchangeRate.nil? || !last_exchangeRate.equals?(exchangeRate))
-      exchangeRate.save
-    end
+    ExchangeRate.exchange_dollar(purchase_value,sale_value, self)
   end
 
   def fortex(agent)
     rate = agent.page.search(self.search)[0].text.split("\n\t\t\t\t\t")[0]
     purchase_value = rate[0..4].to_f
     sale_value = rate[5..9].to_f
-    average_value = ((purchase_value + sale_value)/2).round(2)
-    exchangeRate = ExchangeRate.new(company_id:self.id, currency_id: Currency::DollarID,purchase: purchase_value, sale: sale_value, average: average_value)
-    last_exchangeRate = ExchangeRate.last_of_the_company(self.id)
-    if (last_exchangeRate.nil? || !last_exchangeRate.equals?(exchangeRate))
-      exchangeRate.save
-    end
+    ExchangeRate.exchange_dollar(purchase_value,sale_value, self)
   end
 
   def nixus(agent)
     rate = agent.page.search(self.search)[0].text.split('Dólar')[1].split("\n")
     purchase_value = rate[1].strip.to_f
     sale_value = rate[2].strip.to_f
-    average_value = ((purchase_value + sale_value)/2).round(2)
-    exchangeRate = ExchangeRate.new(company_id:self.id, currency_id: Currency::DollarID,purchase: purchase_value, sale: sale_value, average: average_value)
-    last_exchangeRate = ExchangeRate.last_of_the_company(self.id)
-    if (last_exchangeRate.nil? || !last_exchangeRate.equals?(exchangeRate))
-      exchangeRate.save
-    end
+    ExchangeRate.exchange_dollar(purchase_value,sale_value, self)
   end
 
   def sir(agent)
     rate = agent.page.search(self.search).text.split('Dolares')[1]
     purchase_value = rate.split("\r\n")[1].strip.to_f
     sale_value = rate.split("\r\n")[2].strip.to_f
-    average_value = ((purchase_value + sale_value)/2).round(2)
-    exchangeRate = ExchangeRate.new(company_id:self.id, currency_id: Currency::DollarID,purchase: purchase_value, sale: sale_value, average: average_value)
-    last_exchangeRate = ExchangeRate.last_of_the_company(self.id)
-    if (last_exchangeRate.nil? || !last_exchangeRate.equals?(exchangeRate))
-      exchangeRate.save
-    end
+    ExchangeRate.exchange_dollar(purchase_value,sale_value, self)
   end
 
   def suizo(agent)
     rate = agent.page.search(self.search).text.split("|")
     purchase_value = rate[0].split("\n\t\t\t").last.strip.to_f
     sale_value = rate[1].split("\n")[1].strip.to_f
-    average_value = ((purchase_value + sale_value)/2).round(2)
-    exchangeRate = ExchangeRate.new(company_id:self.id, currency_id: Currency::DollarID,purchase: purchase_value, sale: sale_value, average: average_value)
-    last_exchangeRate = ExchangeRate.last_of_the_company(self.id)
-    if (last_exchangeRate.nil? || !last_exchangeRate.equals?(exchangeRate))
-      exchangeRate.save
-    end
+    ExchangeRate.exchange_dollar(purchase_value,sale_value, self)
   end
 
   def to_s
--- app/models/exchange_rate.rb
+++ app/models/exchange_rate.rb
@@ -7,6 +7,15 @@ class ExchangeRate < ActiveRecord::Base
   	exchangeRate[0]
   end
 
+  def self.exchange_dollar(purchase_value,sale_value, company)
+    average_value = ((purchase_value + sale_value)/2).round(2)
+    exchangeRate = ExchangeRate.new(company_id: company.id, currency_id: Currency::DollarID,purchase: purchase_value, sale: sale_value, average: average_value)
+    last_exchangeRate = ExchangeRate.last_of_the_company(company.id)
+    if (last_exchangeRate.nil? || !last_exchangeRate.equals?(exchangeRate))
+      exchangeRate.save
+    end
+  end
+
   def equals?(exchangeRate)
   	self.purchase == exchangeRate.purchase && self.sale == exchangeRate.sale && self.average == exchangeRate.average
   end
