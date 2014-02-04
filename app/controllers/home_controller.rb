8a5f2a5c617130caa66eaf08dd56f18019d09930
Gaston Arbeletche -VAIRIX- <garbeletche@vairix.com>
2014-02-04 10:55:34 -0200

    change title and description in head

--- app/controllers/home_controller.rb
+++ app/controllers/home_controller.rb
@@ -6,8 +6,8 @@ class HomeController < ApplicationController
   def hisotry_exchange_rate
   	@company = Company.friendly.find params[:company_id]
   	@exchange_rates = @company.exchange_rates.order(created_at: :desc)
-  	@title = "Cotizaciones de la empresa: " + @company.name
-  	@description = "Cotizaciones de la empresa: " + @company.name
+  	@title = "Cotizaciones de la empresa " + @company.name
+  	@description = "Cotizaciones de la empresa " + @company.name
   end
 
 end
